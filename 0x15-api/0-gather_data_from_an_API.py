c1e65954dd4afcd4303f94d3bc1454569571326f
allicpp <58084635+alli-cpp@users.noreply.github.com>
2022-12-07 21:49:56 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID"""
-import requests
-import sys
+"""This module containes a python script that interacts with the rest
+api. For a given employee id, it returns information about his/her
+TODO list progress."""
+from requests import get
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    user_info = get('https://jsonplaceholder.typicode.com/users/{}'.
+                    format(argv[1])).json()
+    user_todos = get('https://jsonplaceholder.typicode.com/todos/',
+                     params={"userId": argv[1]}).json()
+    EMPLOYEE_NAME = user_info.get('name')
+    NUMBER_OF_DONE_TASKS = len([task for task in user_todos
+                               if task['completed']])
+    TOTAL_NUMBER_OF_TASKS = len(user_todos)
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for task in user_todos:
+        if task['completed']:
+            print('\t {}'.format(task['title']))
