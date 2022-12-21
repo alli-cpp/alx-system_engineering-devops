bab089d59878cb65866d46983235ac104e0d2ff9
alli-cpp <alli-cpp@github.com>
2022-12-21 05:06:48 -0800

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
-import json
-import requests
+"""This module contains a python script that interacts with the REST api.
+The script exports a given employee data into a json file format"""
+from requests import get
+from json import dump
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    users = get('https://jsonplaceholder.typicode.com/users')
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    content = {}
+    for user in users.json():
+        USER_ID = str(user.get('id'))
+        user_info = get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(USER_ID)).json()
+        user_todos = get('https://jsonplaceholder.typicode.com/todos/',
+                         params={"userId": USER_ID}).json()
+        USERNAME = user_info.get('username')
+
+        list_info = []
+
+        for task in user_todos:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            task_info = {"task": TASK_TITLE,
+                         "completed": TASK_COMPLETED_STATUS,
+                         "username": USERNAME}
+            list_info.append(task_info)
+
+        content[USER_ID] = list_info
+
+    with open("todo_all_employees.json", "w", encoding="utf8") as f:
+        dump(content, f)
